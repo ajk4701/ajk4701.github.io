16f5d7573585946ae27117bad680d44bfff84957
Audrey Kimball <ajkimball2001@gmail.com>
2026-04-29 23:15:41 +0000

    update weather display logic

--- weather/scripts.js
+++ weather/scripts.js
@@ -5,6 +5,37 @@ let offsetLeftStart;
 let scrollLeftStart;
 let isMoving;
 
+//function to get remote json data
+async function getData(url, options) {
+    try {
+        const response = await fetch(url, options);
+        if (response.ok) {
+            const result = await response.json();
+            return result;
+        } else {
+            throw(response.status);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function updateWeather(data) {
+    console.log(data);
+    document.querySelector("#temp span").textContent = data.current.temp_f;
+    document.querySelector("#condition").textContent = data.current.condition.text;
+    
+    let windSpeed = data.current.wind_mph;
+    let windDir = data.current.wind_dir;
+    document.querySelector("#wind").textContent = windSpeed + " mph " + windDir;
+    
+    document.querySelector("#humidity span").textContent = data.current.humidity;
+
+    
+
+}
+
+//wait for dom to load
 document.addEventListener("DOMContentLoaded", function() {
     scrollingBox = document.querySelector("#predictions");
     isMoving = false;
@@ -33,18 +64,15 @@ document.addEventListener("DOMContentLoaded", function() {
         scrollingBox.scrollLeft = 
         scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
     });
+
+    let sampleURL = "https://tordevries.github.io/477/examples/ajax-api-test/current-forecast.js";
+    let sampleOptions = {};
+
+    getData(sampleURL, sampleOptions).then(function(result) {
+        updateWeather(result);
+    });
+
+
+
 });
 
-async function getData(url, options) {
-    try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-            const result = await response.json();
-            return result;
-        } else {
-            throw response.status;
-        }
-    } catch (error) {
-        console.error(error);
-    }
-}
